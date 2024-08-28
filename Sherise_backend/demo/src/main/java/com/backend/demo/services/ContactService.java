56345320238f744c173fbe31d5ff4eed09af342f
Deeksha <deekshavarshneysasni@gmail.com>
2024-08-28 15:21:55 +0530

    SingupEmail config

--- Sherise_backend/demo/src/main/java/com/backend/demo/services/ContactService.java
+++ Sherise_backend/demo/src/main/java/com/backend/demo/services/ContactService.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-    
+
 @Service
 @RequiredArgsConstructor
 public class ContactService {
